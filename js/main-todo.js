6996e7690b41a7f0f09bea1623e8cd679e8c9fda
HongnamKim <ghdskawkd@naver.com>
2022-11-12 01:51:59 +0900

    02. main todo complete

--- js/main-todo.js
+++ js/main-todo.js
@@ -2,40 +2,63 @@ const mainForm = document.getElementById("main-todo-form");
 const mainInput = document.querySelector("#main-todo-form input");
 const mainToDoText = document.querySelector("#main-todo-text");
 const mainToDoList = document.querySelector("#main-todo");
-const mainToDoReset = document.querySelector("#main-todo-reset");
+const mainToDoEdit = document.querySelector("#main-todo-edit");
 
 //const HIDDEN_CLASS = "hidden";
 const MAINTODO_KEY = "mainToDo";
+const MAINTODO_DATE = "mainToDoDate";
 
-function handleMainSubmit(event) {
+//main todo 및 작성 날짜를 localStorage에 저장
+function saveMainToDo(event) {
   event.preventDefault();
   const mainToDo = mainInput.value;
+  const date = new Date();
+  const mainToDoDate = date.getDate();
+
   localStorage.setItem(MAINTODO_KEY, mainToDo);
+  localStorage.setItem(MAINTODO_DATE, mainToDoDate);
   mainForm.classList.add(HIDDEN_CLASS);
 
   paintMainToDo(mainToDo);
 }
 
+//main todo 출력
 function paintMainToDo(mainToDo) {
   mainToDoList.innerText = mainToDo;
   mainToDoText.classList.remove(HIDDEN_CLASS);
   mainToDoList.classList.remove(HIDDEN_CLASS);
-  mainToDoReset.classList.remove(HIDDEN_CLASS);
+  mainToDoEdit.classList.remove(HIDDEN_CLASS);
 }
 
-function resetMainToDo() {
-  console.log("reset clicked");
+function editMainToDo() {
   localStorage.removeItem(MAINTODO_KEY);
-  location.reload();
+  localStorage.removeItem(MAINTODO_DATE);
+
+  mainForm.classList.remove(HIDDEN_CLASS);
+  mainToDoText.classList.add(HIDDEN_CLASS);
+  mainToDoList.classList.add(HIDDEN_CLASS);
+  mainToDoEdit.classList.add(HIDDEN_CLASS);
+}
+
+function checkDateChange() {
+  const tomorrow = new Date();
+  if (tomorrow.getDate() !== parseInt(localStorage.getItem(MAINTODO_DATE))) {
+    editMainToDo();
+  }
 }
 
 const savedMainToDo = localStorage.getItem(MAINTODO_KEY);
 
 if (savedMainToDo === null) {
   mainForm.classList.remove(HIDDEN_CLASS);
-  mainForm.addEventListener("submit", handleMainSubmit);
+
+  mainForm.addEventListener("submit", saveMainToDo);
+
+  setInterval(checkDateChange, 1000);
 } else {
   paintMainToDo(savedMainToDo);
+
+  setInterval(checkDateChange, 1000);
 }
 
-mainToDoReset.addEventListener("click", resetMainToDo);
+mainToDoEdit.addEventListener("click", editMainToDo);
